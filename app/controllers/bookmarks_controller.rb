5530a0f5e8c7bb74a017ae742bd688902f27df5b
Daniel Shaw <danielshaw71@gmail.com>
2024-05-13 17:22:53 +0100

    Added follow and bookmark button to video page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,4 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: %i[destroy]
-
   def index
     @bookmarks = Bookmark.all
   end
@@ -8,19 +6,20 @@ class BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new(user_id: current_user.id, video_id: params[:video_id])
     if @bookmark.save
-      flash[:notice] = "Successfully bookmarked video."
+      flash[:notice] = 'Successfully bookmarked video.'
     else
-      flash[:alert] = "Unable to bookmark video."
+      flash[:alert] = 'Unable to bookmark video.'
     end
     redirect_back(fallback_location: request.referer || root_path)
   end
 
   def destroy
-  end
-
-  private
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
+    @bookmark = Bookmark.find_by(user_id: current_user.id, video_id: params[:video_id])
+    if @bookmark.destroy
+      flash[:notice] = 'Successfully unbookmarked video.'
+    else
+      flash[:alert] = 'Unable to unbookmark video.'
+    end
+    redirect_back(fallback_location: request.referer || root_path)
   end
 end
