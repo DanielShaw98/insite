eebed284ee26e2a924579eadb6131623f01b2fef
Robbie Bullough <robbie.bullough@hotmail.co.uk>
2024-05-24 12:01:15 +0100

    show/hide on login page, reset password animation change, default url paths to config files, added :focus functionality to :hover devise buttons, adjusted authenticate-bg to make cards centre

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,12 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
+  # Set the default URL host for generated URLs.
+  # This ensures that URL helpers like edit_password_url generate complete URLs
+  # with the correct host included. It's important for emails, redirects,
+  # and other situations where absolute URLs are needed.
+  Rails.application.routes.default_url_options[:host] = 'OUR DOMAIN NAME WILL GO HERE'
+
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
