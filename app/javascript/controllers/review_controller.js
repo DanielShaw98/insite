11c3b8ce23348b2fea668bdf5beb12c1eb0fd5cb
Daniel Shaw <danielshaw71@gmail.com>
2024-12-16 15:32:39 +0000

    Removed comments

--- app/javascript/controllers/review_controller.js
+++ app/javascript/controllers/review_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
         const response = await fetch(`${this.urlValue}?offset=${this.offsetValue}`, {
             method: 'GET',
             headers: {
-                'Accept': 'application/json' // Specify the desired response format
+                'Accept': 'application/json'
             }
         });
 
@@ -19,16 +19,13 @@ export default class extends Controller {
 
         const data = await response.json();
 
-        // Iterate over each review in the JSON response and create HTML elements
         data.reviews.forEach(review => {
-        // Create a div element for the review
         const reviewElement = document.createElement('div');
         reviewElement.classList.add('video-review');
 
         const dateOptions = { year: '2-digit', month: '2-digit', day: '2-digit' };
         const formattedDate = new Date(review.created_at).toLocaleDateString('en-GB', dateOptions);
 
-        // Create the inner HTML for the review element
         reviewElement.innerHTML = `
           <div class="video-review-user-info">
             <div class="video-review-username-date">
@@ -48,16 +45,13 @@ export default class extends Controller {
           <div class="video-review-content">${review.content}</div>
           `;
 
-        // Append the review element to the container
         this.containerTarget.appendChild(reviewElement);
         });
 
-        // Hide the "Show More" button if there are no more reviews to load
         if (!data.has_more) {
             this.showMoreButtonTarget.style.display = "none";
         }
 
-        // Increment offset value for the next request
         this.offsetValue += 3;
     } catch (error) {
         console.error("Error fetching reviews:", error);
